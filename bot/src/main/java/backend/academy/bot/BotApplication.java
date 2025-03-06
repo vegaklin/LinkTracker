43f8dbdd9369784c63cac5a7ebc0dff558f1be5a
vegaklin <ruslan.evn@mail.ru>
2025-03-06 19:44:22 +0300

    fix: edit bot configuration

--- bot/src/main/java/backend/academy/bot/BotApplication.java
+++ bot/src/main/java/backend/academy/bot/BotApplication.java
@@ -1,5 +1,6 @@
 package backend.academy.bot;
 
+import backend.academy.bot.configuration.BotConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
--- bot/src/main/java/backend/academy/bot/configuration/BotConfig.java
+++ bot/src/main/java/backend/academy/bot/configuration/BotConfig.java
@@ -1,9 +1,10 @@
-package backend.academy.bot;
+package backend.academy.bot.configuration;
 
 import jakarta.validation.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record BotConfig(@NotEmpty String telegramToken) {}
+public record BotConfig(@NotEmpty @Value("${app.telegram-token}") String telegramToken) {}
