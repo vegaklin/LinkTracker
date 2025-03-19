d7b0cc2753b91d5648c4ecb152678ca725c12fa8
vegaklin <ruslan.evn@mail.ru>
2025-03-20 01:13:07 +0300

    refactor: spotless apply linux

--- bot/src/main/java/backend/academy/bot/dto/LinkUpdate.java
+++ bot/src/main/java/backend/academy/bot/dto/LinkUpdate.java
@@ -4,9 +4,4 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
-public record LinkUpdate(
-    @NotNull Long id,
-    @NotEmpty String url,
-    String description,
-    @NotNull List<Long> tgChatIds
-) {}
+public record LinkUpdate(@NotNull Long id, @NotEmpty String url, String description, @NotNull List<Long> tgChatIds) {}
